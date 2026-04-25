7a64adfe76fa87b9d58f7e862a65132962f303c3
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2026-04-25 11:16:07 +0200

    refactor: introduce new direction type

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
@@ -7,12 +7,23 @@ namespace SmellyMarsRover
         private string _direction;
         private int _y;
         private int _x;
+        private Direction _newDirection;
 
         public Rover(int x, int y, string direction)
         {
-            _direction = direction;
+            Direction = direction;
             _y = y;
             _x = x;
+        }
+
+        public string Direction
+        {
+            get => _direction;
+            set
+            {
+                _direction = value;
+                _newDirection = new Direction(value);
+            }
         }
 
         public void Receive(string commandsSequence)
@@ -27,41 +38,41 @@ namespace SmellyMarsRover
                 if (rotateLeft)
                 {
                     // Rotate Rover Left
-                    if (_direction.Equals("N"))
+                    if (Direction.Equals("N"))
                     {
-                        _direction = "W";
+                        Direction = "W";
                     }
-                    else if (_direction.Equals("S"))
+                    else if (Direction.Equals("S"))
                     {
-                        _direction = "E";
+                        Direction = "E";
                     }
-                    else if (_direction.Equals("W"))
+                    else if (Direction.Equals("W"))
                     {
-                        _direction = "S";
+                        Direction = "S";
                     }
                     else
                     {
-                        _direction = "N";
+                        Direction = "N";
                     }
                 }
                 else if (rotateRight)
                 {
                     // Rotate Rover Right
-                    if (_direction.Equals("N"))
+                    if (Direction.Equals("N"))
                     {
-                        _direction = "E";
+                        Direction = "E";
                     }
-                    else if (_direction.Equals("S"))
+                    else if (Direction.Equals("S"))
                     {
-                        _direction = "W";
+                        Direction = "W";
                     }
-                    else if (_direction.Equals("W"))
+                    else if (Direction.Equals("W"))
                     {
-                        _direction = "N";
+                        Direction = "N";
                     }
                     else
                     {
-                        _direction = "S";
+                        Direction = "S";
                     }
                 }
                 else
@@ -76,15 +87,15 @@ namespace SmellyMarsRover
 
                     var displacement = displacement1;
 
-                    if (_direction.Equals("N"))
+                    if (Direction.Equals("N"))
                     {
                         _y += displacement;
                     }
-                    else if (_direction.Equals("S"))
+                    else if (Direction.Equals("S"))
                     {
                         _y -= displacement;
                     }
-                    else if (_direction.Equals("W"))
+                    else if (Direction.Equals("W"))
                     {
                         _x -= displacement;
                     }
@@ -98,25 +109,35 @@ namespace SmellyMarsRover
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((Rover)obj);
         }
-
+        
         protected bool Equals(Rover other)
         {
-            return _direction == other._direction && _y == other._y && _x == other._x;
+            return _y == other._y && _x == other._x && Equals(_newDirection, other._newDirection);
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(_direction, _y, _x);
+            return HashCode.Combine(_y, _x, _newDirection);
         }
 
         public override string ToString()
         {
             return $"{nameof(_direction)}: {_direction}, {nameof(_y)}: {_y}, {nameof(_x)}: {_x}";
+        }
+    }
+
+    public record Direction()
+    {
+        private readonly string _direction;
+
+        public Direction(string direction) : this()
+        {
+            _direction = direction;
         }
     }
 }
