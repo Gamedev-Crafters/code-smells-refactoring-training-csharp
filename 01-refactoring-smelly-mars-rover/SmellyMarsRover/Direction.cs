01f8ecd737c70325c33865bd9f6a4fe5d97aa4a1
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2026-04-26 11:59:46 +0200

    refactor: extract direction to own file

---
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Direction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmellyMarsRover;
+
+internal record Direction(string Cardinality)
+{
+    public Direction RotateRight()
+    {
+        return Cardinality switch
+        {
+            "N" => new Direction("E"),
+            "S" => new Direction("W"),
+            "W" => new Direction("N"),
+            "E" => new Direction("S"),
+            _ => throw new Exception("Invalid direction")
+        };
+    }
+}
--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
@@ -104,19 +104,4 @@ namespace SmellyMarsRover
             return $"{"_direction"}: {_direction.Cardinality}, {nameof(_y)}: {_y}, {nameof(_x)}: {_x}";
         }
     }
-
-    internal record Direction(string Cardinality)
-    {
-        public Direction RotateRight()
-        {
-            return Cardinality switch
-            {
-                "N" => new Direction("E"),
-                "S" => new Direction("W"),
-                "W" => new Direction("N"),
-                "E" => new Direction("S"),
-                _ => throw new Exception("Invalid direction")
-            };
-        }
-    }
 }
